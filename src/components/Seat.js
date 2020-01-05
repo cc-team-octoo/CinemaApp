1c0473c7ac9b0d959c2d92f9b7ec5ceed8b1c04f
Aimekram <janina.byszewska@gmail.com>
2020-01-05 21:38:51 +0100

    Add margin and display inline-block

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import styled from 'styled-components';
 
-const Seat = () => {
-    return <div>1A</div>
-};
+const Seat = styled.div`
+    height: 20px;
+    width: 20px;
+    border-radius: 5px;
+    background-color: green;
+    margin: 2px;
+    display: inline-block;
+`;
 
 export default Seat;
