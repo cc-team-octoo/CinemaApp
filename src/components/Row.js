3f0f04a7c30b095fc52edd122adee8f61c8da868
Aimekram <janina.byszewska@gmail.com>
2020-01-06 12:17:12 +0100

    Change seats' ids and keys to begin from 1

--- src/components/Row.js
+++ src/components/Row.js
@@ -4,7 +4,7 @@ import Seat from './Seat';
 const Row = () => {
     let row = [];
     for (let i = 0; i < 10; i++) {
-    row.push(<Seat id={i} key={i}>{i}</Seat>);
+        row.push(<Seat id={i+1} key={i+1}></Seat>);
     }
 return <div>{row}</div>
 };
