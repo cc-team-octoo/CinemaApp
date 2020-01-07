1d94e64bde3de23d3059f3c5adf0ddee8a3b8a7e
Aimekram <janina.byszewska@gmail.com>
2020-01-07 21:12:20 +0100

    Add constructor function and take console log checked seats

--- src/components/Seat.js
+++ src/components/Seat.js
@@ -2,17 +2,26 @@ import React, { Component } from 'react';
 import { StyledSeat, CheckboxContainer, HiddenCheckbox } from './StyledSeat';
 
 class Seat extends Component {
-    state = { checked: false }
+    constructor(props) {
+        super(props);
+        this.state = { checked: false };
+        this.handleCheckboxChange.bind(this);
+    }  
 
-    handleCheckboxChange = event =>
-        this.setState({ checked: event.target.checked })
+    handleCheckboxChange = async event => {
+        await this.setState({ checked: event.target.checked });
+        this.state.checked ? 
+            console.log(`Seat number ${this.props.id} is checked`) : 
+            console.log(`Seat number ${this.props.id} was unchecked`)
+    }        
     
-    render(props) {
+    render() {
         return (
             <CheckboxContainer
-                checked={this.state.checked}
-                onChange={this.handleCheckboxChange}>
-                    <HiddenCheckbox checked={this.state.checked} />
+                checked={this.state.checked}>
+                    <HiddenCheckbox 
+                        checked={this.state.checked}
+                        onChange={this.handleCheckboxChange}/>
                     <StyledSeat checked={this.state.checked}>{this.props.id}</StyledSeat>
             </CheckboxContainer>
         ) 
