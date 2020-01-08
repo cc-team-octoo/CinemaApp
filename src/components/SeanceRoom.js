99c13185737c237b0b63a999742674340213b60a
Aimekram <janina.byszewska@gmail.com>
2020-01-08 19:19:29 +0100

    Add unique keys to generated seats and rows

--- src/components/SeanceRoom.js
+++ src/components/SeanceRoom.js
@@ -6,7 +6,9 @@ const SeanceRoom = () => {
     let room = [];
     let rows = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
     for (let i = 0; i < 8; i++) {
-        room.push(<Row rowName={rows[i]}/>)
+        room.push(<Row 
+            key={`row${i}`}
+            rowName={rows[i]}/>)
     };
 
     return <StyledRoom>{room}</StyledRoom>
