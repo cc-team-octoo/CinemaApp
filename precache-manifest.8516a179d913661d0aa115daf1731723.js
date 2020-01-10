b00e754d0df740f2925235460195e997de7c8ade
Mirosław Biedrowski <biedrowski.miroslaw@wp.pl>
2020-01-10 22:12:36 +0100

    Updates

---
+++ precache-manifest.8516a179d913661d0aa115daf1731723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5e89b72d54d1d56e2ade43e145c66c",
+    "url": "/CinemaApp/index.html"
+  },
+  {
+    "revision": "6456941b8132a0666ae8",
+    "url": "/CinemaApp/static/css/main.29a56fb2.chunk.css"
+  },
+  {
+    "revision": "66c0fc61bdba5e4ce5d0",
+    "url": "/CinemaApp/static/js/2.9c4d8059.chunk.js"
+  },
+  {
+    "revision": "7eb906b2d8ef627da08d7f4a543e6732",
+    "url": "/CinemaApp/static/js/2.9c4d8059.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6456941b8132a0666ae8",
+    "url": "/CinemaApp/static/js/main.99ee264f.chunk.js"
+  },
+  {
+    "revision": "1ac21b8c8521094c4bf6",
+    "url": "/CinemaApp/static/js/runtime-main.16ea2d5c.js"
+  }
+]);
